c1279961402e0d9a8c8130f40f5dff46cfde475e
per1shed <ptisenko@gmail.com>
2026-08-20 16:59:35 +0300

    new

--- dashboard/chart.py
+++ dashboard/chart.py
@@ -203,7 +203,7 @@ class ChartRenderer:
                     va="bottom",
                     fontsize=t.bar_label_size,
                     color=color,
-                    fontweight="bold",
+                    fontweight="semibold",
                     zorder=6,
                     clip_on=False,
                 )
